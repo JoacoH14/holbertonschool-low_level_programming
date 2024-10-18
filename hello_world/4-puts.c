892d255181d7614435d0ed271d83f015cfb2d4c4
JoacoH14 <joaco_hernandez00@hotmail.com>
2024-10-18 17:05:17 +0000

    p

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -4,11 +4,8 @@
 */
 int main(void)
 {
-/* uso puts para mostrar el mensaje 
-*/
-
+/* uso puts para mostrar el mensaje */
 char *str1 = "Programming is like building a multilingual puzzle"
 	puts(str1)
-
 return 0
 }
