b07e91abe5bae2065d7f41890c0cbc32027855a3
JoacoH14 <joaco_hernandez00@hotmail.com>
2024-10-21 15:40:39 +0000

    p

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,18 +1,23 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/*
+ * main - nada
+ * Return: 0
+**/
+/*
+*sentencia if combinado con printf muestra la salida dependiendo del valor de n
+ **/
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n > 0) 
-	{printf (n + "es positivo")
-	}
-	else{printf (n + "es negativo")}
-	while(n = 0, printf (n + "es cero"))
+	if (n > 0)
+	{printf(n + "es positivo");
+	};
+	else
+	{printf(n + "es negativo")};
+	while (n = 0, printf(n + "es cero"))
 	return (0);
 }
