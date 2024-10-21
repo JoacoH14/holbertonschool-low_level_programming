f81a615f6766324803e277ffc9a39dda545bd803
JoacoH14 <joaco_hernandez00@hotmail.com>
2024-10-21 14:20:33 +0000

    p

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,11 +8,10 @@ int main(void)
 	/**
 	 *uso printf en cada linea para mostrar cada tipo de dato
 	*/
-	printf("Size of int: %zu byte(s)", sizeof(int));
-    printf("Size of float: %zu byte(s)", sizeof(float));
-    printf("Size of double: %zu byte(s)", sizeof(double));
-    printf("Size of long: %zu byte(s)", sizeof(long));
-    printf("Size of long long: %zu byte(s)", sizeof(long long));
+	printf("Size of int: %zu byte(s)\n", sizeof(int));
+    printf("Size of float: %zu byte(s)\n", sizeof(float));
+    printf("Size of long: %zu byte(s)\n", sizeof(long));
+    printf("Size of long long: %zu byte(s)\n", sizeof(long long));
 
 	return (0);
 }
