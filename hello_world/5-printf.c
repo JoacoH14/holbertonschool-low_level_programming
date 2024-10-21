6417f7254fab0ff9b2708402f94a081c682591dd
JoacoH14 <joaco_hernandez00@hotmail.com>
2024-10-21 13:39:38 +0000

    p

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - nada
+ * return 0
+ */
+int main(void){
+	/**uso la funcion printf para mostrar en pantalla el msj
+	 */
+printf("with proper grammar, but the outcome is a piece of art");
+return (0);
+
+
+}
