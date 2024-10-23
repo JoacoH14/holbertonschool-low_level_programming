d6b82fb1e6f447b2324e7bbbca6737d43b7eab70
JoacoH14 <joaco_hernandez00@hotmail.com>
2024-10-23 17:35:52 +0000

    p

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+ * _islower - nada
+ * @c: es una variable que almacena un caracter en ASCII
+ * Return: 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+		else
+		{
+	return (0);
+	}
+	}
