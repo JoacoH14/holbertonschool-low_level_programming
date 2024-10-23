a1d424dc26082dfe51b44f52d490e85da88f7d30
JoacoH14 <joaco_hernandez00@hotmail.com>
2024-10-23 14:22:25 +0000

    p

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,15 +1,9 @@
-#include<unistd.h>
-/**
- * main - nada
- * Return: 0
- */
+#include"main.h"
+
 
 int main(void)
 {
-	const char str[] = "_putchar\n";
+	putchar("_putchar\n")
 
-	write(1, str, sizeof(str) - 1);
-
-
-	return (0);
+		return (0);
 }
