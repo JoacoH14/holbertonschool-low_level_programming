f61e2d8a4a07536824dd10d764afda6b86126af2
JoacoH14 <joaco_hernandez00@hotmail.com>
2024-10-24 14:28:08 +0000

    p

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include"main.h"
+/**
+ * print_sign - vacio
+ * @n: n es una variable que almacena un caracter
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+	return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+		(n == 0);
+	{
+		_putchar('0');
+		return (0);
+}
+}
