21d7248c06cbfe39eeb772c7f45b5d9ebff6878c
JoacoH14 <joaco_hernandez00@hotmail.com>
2024-10-28 17:39:50 +0000

    p

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include"main.h"
+/**
+ * add - nada
+ * @a: variable que almacena un valor numerico
+ * @b: variable que almacena un valor numerico
+ * Return: 0
+ */
+int add(int a, int b)
+{	
+	return (a + b);
+}
---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,10 @@
+#include"main.h"
+
+void jack_bauer(void)
+{
+	int hour, minute;
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			_putchar()
