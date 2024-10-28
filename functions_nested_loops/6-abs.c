c841c37062add6c2a17d089ab70e140fb2662a63
JoacoH14 <joaco_hernandez00@hotmail.com>
2024-10-28 13:42:42 +0000

    p

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,9 +1,14 @@
 #include"main.h"
-
+/**
+ * _abs - nada
+ * numero - variable que almacena un numero
+ */
 int _abs(int numero)
-{	if (numero >= 0)
+{
+	if (numero >= 0)
 	{ return (numero);
 	}
 	else
-	{ return (-numero)
+	{ return (-numero);
+}
 }
