c54a2e0a2b8fb286c608b484c099c5c75055db4a
JoacoH14 <joaco_hernandez00@hotmail.com>
2024-10-28 17:55:09 +0000

    p

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+ * print_to_98(int n)
+ * @n: variable que almacena valor numerico
+ * Return: 0
+ */
+void print_to_98(int n)
+{
+	for (n = n; n <= 97; n++)
+	{
+		printf("%d, ", n)
+	}
+	for (n = n; n >= 99; n--)
+		printf("%d, ", n)
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -7,3 +7,4 @@ int print_sign(int n);
 int _abs(int numero);
 int print_last_digit(int numero);
 int add(int, int);
+void print_to_98(int n);
