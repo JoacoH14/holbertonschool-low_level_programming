1f6a9f3f11bd774218da419fb2cdf7742f0571a2
JoacoH14 <joaco_hernandez00@hotmail.com>
2024-10-30 17:21:31 +0000

    p

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -8,10 +8,10 @@ void print_most_numbers(void)
 
 		for (num = '0'; num <= '9'; num++)
 		{
-			_putchar(num);
-		}
-			if (num == '2' || num == '4')
+			if (num != '2' || num != '4')
 			{
-				_putchar(' ');
+		_putchar(num + '0');
 			}
+	}
+	_putchar('\n');
 	}
