b23920eacc728462ed5228f5a115bf96d56750ec
JoacoH14 <joaco_hernandez00@hotmail.com>
2024-10-30 17:59:07 +0000

    p

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,18 +1,21 @@
 #include"main.h"
-
+/**
+ * more_numbers - nada
+ */
 void more_numbers(void)
 {
 	int num, nam = 0;
-		for (nam = 0; nam <= 9; nam++)
-		{
+
+	for (nam = 0; nam <= 9; nam++)
+	{
 		for (num = 0; num <= 14; num++)
+		{
+			if (num >= 10)
 			{
-				if (num >= 10)
-				{
-					_putchar('1');
-				}
-				_putchar(num % 10 + '0');
+				_putchar('1');
 			}
-		_putchar('\n');
+			_putchar(num % 10 + '0');
 		}
+		_putchar('\n');
+	}
 }
