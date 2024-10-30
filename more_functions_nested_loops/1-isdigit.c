0ec0415ac8ec33de67ac73cfcff042c6a373a953
JoacoH14 <joaco_hernandez00@hotmail.com>
2024-10-30 14:10:13 +0000

    p

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ * _isdigit - nada
+ * @c: es una variable que almacena un dato numerico
+ * Return: 0
+ */
+int _isdigit(int c)
+{
+	if (c <= 9 && c >= 0)
+	{
+		return (1);
+	}
+		else
+		{
+			return (0);
+		}
+}
