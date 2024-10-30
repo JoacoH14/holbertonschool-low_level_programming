6ee05129864ede01f2910e7c1a596cd7e960fa48
JoacoH14 <joaco_hernandez00@hotmail.com>
2024-10-30 14:39:13 +0000

    p

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,8 +1,14 @@
 #include"main.h"
-
+/**
+ * mul - nada
+ * @a: variable numerica
+ * @b: variable numerica
+ * Return: 0
+ */
 int mul(int a, int b)
 {
 	int c;
-	a * b = c
+	a * b;
+	c = a * b;
 		return(c);
 }
