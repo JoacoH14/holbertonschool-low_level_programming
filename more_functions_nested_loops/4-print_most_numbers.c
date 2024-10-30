9f62a8771d45bec9b8cc04fceca97397d3d9d1c4
JoacoH14 <joaco_hernandez00@hotmail.com>
2024-10-30 17:03:58 +0000

    p

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+ * print_most_numbers - nada
+ */
+void print_most_numbers(void)
+	{
+		int num
+		for (num = '0'; num <= '9'; num++)
+		{
+			_putchar(num);
+		}
+			if (num == '2' || num == '4')
+			{
+				_putchar(' ');
+			}
+	}
