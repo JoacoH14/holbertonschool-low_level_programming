9c44c0f3d62d355883e15026bfbc51a479065167
JoacoH14 <joaco_hernandez00@hotmail.com>
2024-11-04 02:29:38 +0000

    p

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+void rev_string(char *s)
+{
+	int i;
+	int length = 0;
+
+	while(s[length] != '\0')
+	{
+		length++;
+	}
+	for (i = 0; i < length / 2; i++)
+	{
+		char temp = s[i];
+		s[i] = s[length - 1 - i];
+		s[length - 1 - i] = temp;
+	}
+}
