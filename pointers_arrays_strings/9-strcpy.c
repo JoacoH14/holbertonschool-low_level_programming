86a263267aedfaac38328bcbd9cebe4883991c46
JoacoH14 <joaco_hernandez00@hotmail.com>
2024-11-04 14:10:47 +0000

    p

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ * strcpy - nada
+ * @dest: variable
+ * @src: variable
+ */
+char *_strcpy(char *dest, const char *src)
+{
+	int index = 0;
+
+	while (src[index])
+	{
+		dest[index] = src[index];
+		index++;
+	}
+	return (dest);
+}
