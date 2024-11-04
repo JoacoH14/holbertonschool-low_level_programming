4a3d1afbce49aab5cc8d989eb0886a382021e42e
JoacoH14 <joaco_hernandez00@hotmail.com>
2024-11-04 02:07:24 +0000

    p

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+ * print_rev - nada
+ * @s: variable
+ */
+void print_rev(char *s)
+{
+	int i;
+	int length = strlen(s);
+	
+	for (i = length - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
