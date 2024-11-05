d20a1ff93cc82b9ae0969318431ca54136b1c443
JoacoH14 <joaco_hernandez00@hotmail.com>
2024-11-05 14:06:58 +0000

    p

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,19 +1,18 @@
 #include"main.h"
-
+/**
+ * _strcat - nada
+ * @dest: variable
+ * @src: variable
+ * Return: dest
+ */
 char *_strcat(char *dest, char *src)
 {
-	while (*dest != '\0')
-	{
-		dest++;
-	}
+	int index = 0, dest_len = 0;
 
-	while (*src != '\0')
-	{
-		*dest = *src;
-		dest++;
-		src++;
-	}
-	*dest = '\0';
+	while (dest[index++])
+		dest_len++;
 
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
 	return (dest);
 }
