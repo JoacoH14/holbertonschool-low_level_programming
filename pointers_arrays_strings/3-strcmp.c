bedb36d190c755a2764a73686742cfeabcc0de64
JoacoH14 <joaco_hernandez00@hotmail.com>
2024-11-06 15:31:15 +0000

    p

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include"main.h"
+/**
+ * _strcmp - nada
+ * @s1: variable
+ * @s2: variable
+ * Return: dest
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int index = 0, src_len = 0;
+
+	while (src[index++])
+		src_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
+
+	for (index = src_len; index < n; index++)
+		dest[index] = '\0';
+	return (dest);
+}
