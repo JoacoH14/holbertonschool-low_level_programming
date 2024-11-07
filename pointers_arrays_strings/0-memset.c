6987e635ccbfed774e9e5bc72fc7c884c2eaa270
JoacoH14 <joaco_hernandez00@hotmail.com>
2024-11-07 18:13:07 +0000

    p

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,5 +1,17 @@
 #include"main.h"
-
+/**
+ * _memset - nada
+ * @s: variable
+ * @b: variable
+ * @n: variable
+ * Return: memory
+ */
 char *_memset(char *s, char b, unsigned int n)
-{
+{	
+	unsigned int index;
+	unsigned char *memory = s, value = b;
 
+	for (index = 0; index < n; index++)
+		memory[index] = value;
+	return (memory);
+}
