b8daf5d91917f56407da2a30ae523e36f6dd705d
JoacoH14 <joaco_hernandez00@hotmail.com>
2024-11-08 14:38:22 +0000

    p

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+ * _memset - nada
+ * @s: variable
+ * @b: variable
+ * @n: variable
+ * Return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++
+			*(s + i) = b;
+			return (s);
+}
