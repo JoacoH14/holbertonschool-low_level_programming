65c7fdc07c8393823ceb7c83c29ddbfab78aabe9
JoacoH14 <joaco_hernandez00@hotmail.com>
2024-11-11 02:40:37 +0000

    p

---
+++ argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include"main.h"
+/**
+ * main - nada
+ * @argc: variable
+ * @argv: variable
+ * Return: 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
