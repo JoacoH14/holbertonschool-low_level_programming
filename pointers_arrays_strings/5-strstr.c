816dc3eaaa0a9166c348c44f4bcc32d8dd1a7629
JoacoH14 <joaco_hernandez00@hotmail.com>
2024-11-11 01:08:41 +0000

    p

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include"main.h"
+/**
+ * _strstr - nada
+ * @haystack: variable
+ * @needle: variable
+ * Return: 0
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *haystackb;
+	char *needleb;
+
+	while (*haystack != '/0')
+	{
+		haystackb = haystack;
+		needleb = needle;
+
+		while (*haystack != '/0' && *needleb != '/0' && *haystack == *needleb)
+		{
+			haystack++;
+			needleb++;
+		}
+		if (!*needleb)
+			return (haystackb);
+		haystack = haystackb + 1;
+	}
+	return (0);
+}
