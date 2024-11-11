209d3c8a5009f7e4c595c1d0504418da0e818531
JoacoH14 <joaco_hernandez00@hotmail.com>
2024-11-11 01:20:03 +0000

    p

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include"main.h"
+/**
+ * print_chessboard - nada
+ * @a: variable
+ */
+void print_chessboard(char (*a)[8])
+{
+	unsigned int 1, m = 0;
+
+	for (i = 0; i < 64; i++)
+	{
+		if (i % 8 == 0 && i != 0)
+		{
+			m = i;
+			_putchar('\n');
+		}
+		_putchar(a[i / 8][i - m]);
+	}
+	_putchar('\n');
+}
