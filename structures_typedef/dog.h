85f91c007641b9d29a2aebcd8e81e4623dad639c
JoacoH14 <joaco_hernandez00@hotmail.com>
2024-11-25 01:13:34 +0000

    p

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,16 +1,27 @@
-#include"main.h"
+#ifndef _DOG_H_
+#define _DOG_H_
+
 /**
- * init_dog - nada
- * @d: variable
- * @age: variable
- * @owner: variable
+ * dog_t - Typedef for struct dog
  */
-void init_dog(struct dog *d, char *name, float age, char *owner)
+	typedef struct dog dog_t;
+
+	/**
+	 * struct dog - struct that stores info of the dog
+	 * @name: name of the dog
+	 * @age: age of the dog
+	 * @owner: owner of the dog
+	 */
+	struct dog
 {
-	if (d)
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
-}
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
